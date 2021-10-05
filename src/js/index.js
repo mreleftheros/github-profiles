974c4a336cd61ae879b52406b28fc91621ed65ae
eleftheros <eleftheros_all@protonmail.com>
2021-10-05 11:12:58 +0300

    Add getUser async function

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 const searchForm = document.getElementById("searchForm");
 
+// search for user data on github api and call createCard function with returned data
+const getUser = async name => {
+  const baseUrl = "https://api.github.com/users/";
+  const res = await fetch(baseUrl + name);
+  const data = await res.json();
 
+  return createCard(data);
+}
 
 // submits search form and calls getUser with username
 const submitSearchForm = e => {
